03d4d91a6f71e7b359afafa927bb87309b82072a
Jonathan Wilkinson <jonny@ctrl-hub.com>
2019-07-04 09:33:25 +0100

    Make treebuilder configuration backwards compatible with older Symfony versions

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,8 +18,9 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('ashley_dawson_glide');
-
-        $rootNode = $treeBuilder->getRootNode();
+        $rootNode = \method_exists($treeBuilder, 'getRootNode')
+            ? $treeBuilder->getRootNode()
+            : $treeBuilder->root('ashley_dawson_glide');
 
         $rootNode
             ->children()
